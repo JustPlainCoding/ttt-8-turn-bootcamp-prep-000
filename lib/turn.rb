a2b3e0416fa8c08bc7f867dd062d00ee39fd07db
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-26 22:59:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,10 +4,10 @@ end
 def input_to_index(input)
 end
 
-def valid_move?(index)
+def valid_move?(board, index)
 end
 
-def move(board, index)
+def move(board, index, player = "X")
 end
 
 def turn(board)
