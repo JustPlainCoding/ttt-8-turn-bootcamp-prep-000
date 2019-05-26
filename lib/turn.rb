9cb238f92eac955a4a0309658d45fd6b8d04acb9
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-26 23:10:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def input_to_index(input)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_valid(board[index])
+  if index.between?(0,8) && !position_valid?(board[index])
     true
   end
 end
